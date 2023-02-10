e4018eed76e36e7ac8250014b50c25d3c82e16d1
EidHassanx1 <eid-hassan@outlook.com>
2023-02-10 13:07:31 +0100

    10.02

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
-import Todos from './components/Todos';
 import Navbar from './components/Navbar';
+import Todos from './components/Todos';
 import Form from './components/Form';
+import { useState } from 'react';
 
 const todoStart = [
   {
     id:0,
-    title: "lager Middag",
-    content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+    title: "Lage middag",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non orci tellus. Integer eget molestie tortor.",
+
   }
 ]
 
 function App() {
+  const [todo, setTodo] = useState({title: "", content: ""})
+  const [todos, setTodos] = useState([...todoStart])
+  const [id, setId] = useState([1])
+
+  const addToDo = (event) => {
+    const inputName = event.target.name
+    const inputValue = event.target.value
+    setTodo((prev) => ({...prev, [inputName]: inputValue}))
+  }
+
+  const handleClick = () => {
+    setId((prev) => prev + 1)
+    setTodos((prev) => [
+      {id, title: todo.title, content: todo.content}, ...prev
+    ])
+  }
+
+  const removeToDos = (todoid) => {
+    setTodos(todos.filter((item) => item.id !== todoid))
+    console.log(todos)
+  }
+  console.log(todos)
+
   return (
     <>
-    <Navbar />
-    <Form />
-    <Todos todoStart={todoStart}/>
+    <Navbar username="Hassan"/>
+    <section className='container'>
+    <Form handleClick={handleClick} addToDo={addToDo} todo={todo}/>
+    </section>
+    <Todos todoStart={todos} removeToDos={removeToDos}/>
     </>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,25 @@
-export default function Form() {
-    const handleSubmit = (event) => {
+export default function Form({handleClick, todo, addToDo}){
+
+    const handleSubmit = (event) =>{
         event.preventDefault()
     }
-    return (
+    return(
         <form onSubmit={handleSubmit}>
-            <label htmlfor="to-do">To do</label>
-            <input
-            name="title"
-            id="to-do"
-            type={text}/>
+      <label htmlFor="to-do">To do</label>
+       <input 
+       name="title"
+       id="to-do"
+       type="text"
+       value={todo.addToDo}
+       onChange={addToDo}/>
+       <label htmlFor="content">Content</label>
+       <textarea
+       name="content"
+       id="content"
+       type="text"
+       value={todo.addToDo}
+       onChange={addToDo}/>
+       <button className="todo-btn" type="submit" onClick={handleClick}>Add to do</button>
         </form>
     )
 }
